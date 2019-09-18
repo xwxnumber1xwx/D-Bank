413690897a35e9dbbf1c7437edd0782e3907cb55
Daniele Caputo <32809282+xwxnumber1xwx@users.noreply.github.com>
2019-09-18 11:36:44 +0200

    add real_estate DB

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const button = document.getElementById('search-button');
+const result = document.getElementById('result');
+const forecast = document.getElementById('limit');
+
 async function getUserInfo() {
-    const button = document.getElementById('search-button');
-    const resultContainer = document.getElementById('result-container');
     button.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>  Searching...';
     let query = document.getElementById('user_id').value;
     let messageResult = '';
     let messageForecast = '';
-    const result = document.getElementById('result');
-    const forecast = document.getElementById('limit');
     if (query) {
         await fetch('./api/user_info', {
             method: "POST",
@@ -18,26 +18,29 @@ async function getUserInfo() {
         })
             .then(res => res.json())
             .then(content => {
-                resultContainer.classList.remove('hidden');
-                resultContainer.classList.add('animated');
-                
                 console.log('content', content);
                 if (content[0]) {
                     messageResult = `
-                                <li>User ID: ${query}</li>
-                                <li>Category: ${content[0].cust_catag}</li>
-                                <li>Bank interest: ${content[0].interest}%</li>
-                                <li>Risk: ${content[0].risk}%</li>
-                                <li>Salary range: ${content[0].salary.min}$ - ${content[0].salary.max}$</li>
+                    <h2>User ID ${query}</h2>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                            <td><span class="font-weight-bold">Category</span></td>
+                            <td><span class="font-weight-bold">Bank interest</span></td>
+                            <td><span class="font-weight-bold">Risk</span></td>
+                            <td><span class="font-weight-bold">Salary range</span></td>
+                            </tr>
+                            <tr>
+                            <td>${content[0].cust_catag}</td>
+                            <td>${content[0].interest}%</td>
+                            <td>${content[0].risk}%</td>
+                            <td>${content[0].salary.min}$ - ${content[0].salary.max}$</td>
+                            </tr>
+                        </tbody>
+                        </table>
                                 `
-                    messageForecast = `
-                                <li>Credit Limit: ${content[0].credit_limit}$</li>
-                                <p>Whit this Credit you can buy...</p>
-                                <figure>
-                                <img src="${content[0].image.src}" alt="${content[0].image.alt}"></img>
-                                <figcaption>${content[0].image.alt}</figcaption>
-                                </figure>
-                                `
+                    messageForecast = `<button onClick="findHouses(${content[0].credit_limit})">search</button>`
+
                 } else if ('error' in content) {
                     messageForecast = '';
                     messageResult = `<li>${content.error}</li>`
@@ -48,17 +51,45 @@ async function getUserInfo() {
                                 <li>Sorry, no user found in the Database</li>
                                 `
                 }
+
             }
             )
             .catch(err => console.log('ERROR', err));
-
-
-
-    } else {
-        resultContainer.classList.add('hidden');
-        resultContainer.classList.remove('animated');
     }
+
+
     result.innerHTML = messageResult;
     forecast.innerHTML = messageForecast;
     button.innerHTML = 'Search';
+}
+
+async function findHouses(creditLimit) {
+    await fetch('./api/real_estate?credit_limit=' + creditLimit)
+        .then(data => data.json())
+        .then(results => {
+            let div = document.createDocumentFragment();
+            console.log(results)
+            results.forEach(item => {
+                let card = document.createElement('div');
+                card.classList.add('card', 'small-size');
+                if (item.attachments.images[0]) {
+                    let img = document.createElement('img');
+                    img.setAttribute('src', item.attachments.images[0]);
+                    img.classList.add('card-img-top');
+                    let desc = document.createElement('div');
+                    desc.classList.add('card-body');
+                    let p = document.createElement('p');
+                    p.classList.add('card-text');
+                    p.innerText = `${item.address.city} - ${item.price}$`;
+                    desc.appendChild(p);
+                    card.appendChild(img);
+                    card.appendChild(desc);
+                    div.appendChild(card);
+                    console.log('item', item);
+                }
+            })
+            forecast.innerText = '';
+            forecast.appendChild(div);
+        })
+        .catch(err => console.log(err))
 }
--- server.js
+++ server.js
@@ -10,41 +10,56 @@ app.use(bodyParser.urlencoded({
 
 // MongoDb credentials
 const URL = "mongodb+srv://ibmpod:XXuaRmIfm1pyKzky@d-bank-rv4np.mongodb.net/test?retryWrites=true&w=majority";
-const DB = 'catalog';
-const COLLECTION = 'customer-catalog';
+
 
 /* serves main page */
 app.get("/", function (req, res) {
     res.sendfile('index.html')
 });
 
+/* API get house list */
+app.get("/api/real_estate", function(req, res) {
+    const DB = 'real_estates';
+    const COLLECTION = 'real_estates_catalog';
+    const query = { price: { $lte : +req.query.credit_limit }};
+    mongoConnect(URL, query, DB, COLLECTION, res);
+
+})
+
+/* API get user info */
 app.post("/api/user_info", function (req, res) {
+    const DB = 'catalog';
+    const COLLECTION = 'customer-catalog';
     const query = { cust_catag: req.body.query };
     console.log('connecting, req, res', req, res);
     /* Connect to Mongo DB */
-    const client = new MongoClient(URL, { useNewUrlParser: true });
+    mongoConnect(URL, query, DB, COLLECTION, res)
+});
+
+function mongoConnect(url, query, db, coll, response) {
+    const client = new MongoClient(url, { useNewUrlParser: true });
     client.connect(err => {
         console.log('connect, err', err);
         if (!err) {
-            const collection = client.db(DB).collection(COLLECTION);
+            const collection = client.db(db).collection(coll);
             console.log('collection', collection);
             // perform actions on the collection object
-            collection.find(query).toArray(function (err, result) {
+            collection.find(query).limit(3).toArray(function (err, result) {
                 if (!err) {
                     client.close();
-                    res.status(200).send(result);
+                    response.status(200).send(result);
                 } else {
-                    res.status(500).send({error: 'Connection to MongoDB failed'});
+                    response.status(500).send({error: 'Connection to MongoDB failed'});
                 }
 
             });
             //res.send(JSON.stringify(collection));
         } else {
-            res.status(500).send({error: 'Connection to MongoDB failed'});
+            response.status(500).send({error: 'Connection to MongoDB failed'});
         }
 
     });
-});
+}
 
 /* serves all the static files */
 app.get(/^(.+)$/, function (req, res) {
